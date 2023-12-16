85e77a8bd9fff1f95cdf7049826792a29141f244
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2023-12-16 07:56:37 -0500

    Update assignment 2

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,15 +1,10 @@
 package com.coderscampus;
 
-import java.util.Random;
-
 public class Assignment2Application {
 
 	public static void main(String[] args) {
 		
-		// Random Number Generator from 1-100. 
-		// random.nextInt(100) would be 0-99, so add 1 and we have 1-100.
-		Random random = new Random();
-		int randomNum = random.nextInt(100) + 1;
+		Exercise3Method randomNumGen = new Exercise3Method();
+		randomNumGen.whileLoop();
 	}
-
 }
---
+++ src/com/coderscampus/Exercise3Method.java
@@ -0,0 +1,57 @@
+package com.coderscampus;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise3Method {
+
+	void whileLoop () {
+		// Random Number Generator from 1-100. 
+		// random.nextInt(100) would be 0-99, so add 1 and we have 1-100.
+		Random random = new Random();
+		int randomNum = random.nextInt(100) + 1;
+				
+		// User picks a number 
+				
+		Scanner keyboard = new Scanner(System.in);
+				
+		System.out.println("Pick a number between 1 and 100.");
+		int input = keyboard.nextInt();
+				
+		//Players only receive 5 guesses within range
+		int guess = 1;
+		while (guess < 5) {
+			//If guess is outside of range
+			if (input < 1 || input > 100) {
+			System.out.println("Your guess is not between 1 and 100, please try again.");
+			input = keyboard.nextInt();
+		}
+			//If guess is inside of range
+			else if (input >= 1 && input <= 100) {
+				
+				if (input < randomNum) {
+					System.out.println("Please pick a higher number.");
+					guess++;
+					input = keyboard.nextInt();
+				}
+				else if (input > randomNum) {
+					System.out.println("Please pick a lower number.");
+					guess++;
+					input = keyboard.nextInt();
+				}
+				else if (input == randomNum) {
+					break;
+				}
+					
+			}
+		} 
+		//Either the user has won or lost
+		if (input == randomNum) {
+			System.out.println("You win!");
+		}
+		else {
+		System.out.println("You've run out of guesses. The number was " + randomNum + ".");
+		} 
+	}	
+		
+}
